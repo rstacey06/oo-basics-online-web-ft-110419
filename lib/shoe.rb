fddaf79c7fa92111209025f5dc29b30c339799b7
Burbage Stacey <rstacey06@gmail.com>
2019-11-11 18:58:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,10 @@
 class Shoe 
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand 
+  end
+
 end
+
